1f9e9c8d4f5b7668aa44ef8868f956f8bb7e8dac
Diptayan Jash <djdiptayan1@gmail.com>
2025-05-14 21:41:50 +0530

    Add transaction routes to the API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const healthRoute = require('./health.routes');
+const transactionRoute = require('./transaction.routes');
 
 const router = express.Router();
 
 const apiPrefix = '/api';
 
 router.use(`${apiPrefix}/health`, healthRoute);
+router.use(`${apiPrefix}/transactions`, transactionRoute);
 
 module.exports = router;
