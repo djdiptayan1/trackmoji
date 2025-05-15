bde1712a180e99d4cae3939077977b96e60015e3
Diptayan Jash <djdiptayan1@gmail.com>
2025-05-15 16:42:33 +0530

    Add users route to the main API router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const healthRoute = require('./health.routes');
 const transactionRoute = require('./transaction.routes');
+const usersRoute = require('./users.routes');
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ const apiPrefix = '/api';
 
 router.use(`${apiPrefix}/health`, healthRoute);
 router.use(`${apiPrefix}/transactions`, transactionRoute);
+router.use(`${apiPrefix}/users`, usersRoute);
 
 module.exports = router;
